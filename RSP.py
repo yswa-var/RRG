47681249c4909210e66e78f86551298580866136
Yahaswa Varshney <yswa.var@gmail.com>
2024-06-27 14:41:08 +0530

    feat: new rrg methode

--- RSP.py
+++ RSP.py
@@ -24,3 +24,38 @@ df = df.dropna(axis=1, how='all')
 
 bench_mark = yf.download(index, period='1y')
 
+for i in df.columns:
+    df[i] = normalized_mean(df, i)
+df['nifty'] = normalized_mean(bench_mark, 'Close')
+df = df.dropna(how='all')
+
+rrg = pd.DataFrame()
+for i in df.columns:
+    rrg[f'{i}.rs'] = df['nifty']
+
+for i in df.columns:
+    rrg[f'{i}.rsm'] = rrg[f'{i}.rs'].pct_change(fill_method=None)*100
+
+
+#------------------
+import plotly.graph_objects as go
+
+def plot_relative_strength(df, index_name):
+  fig = go.Figure()
+
+  fig.add_trace(go.Scatter(
+      x=df[f'{index_name}.rs'],
+      y=df[f'{index_name}.rsm'],
+      mode='lines',
+      name=f'Relative Strength of {index_name}'
+  ))
+
+  fig.update_layout(
+      title=f'Relative Strength of {index_name} vs. Nifty',
+      xaxis_title='Date',
+      yaxis_title='Relative Strength'
+  )
+
+  fig.show()
+
+plot_relative_strength(rrg.copy(), '^CNXCONSUM')
